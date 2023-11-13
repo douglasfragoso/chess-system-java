56ada2267b75bd337c03ae754597f28779251083
Douglas Fragoso <douglas.iff@gmail.com>
2023-11-12 22:24:28 -0300

    Checkmate logic

--- scr/application/Program.java
+++ scr/application/Program.java
@@ -16,7 +16,7 @@ public class Program {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
 
@@ -48,7 +48,7 @@ public class Program {
             }
                 
         }
-        
-        
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
